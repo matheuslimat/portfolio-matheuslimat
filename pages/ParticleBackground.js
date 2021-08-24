05c0994d5718e23e1282592f556a50df2beae3bf
matheus.tavares <matheus.tavares@cwi.com.br>
2021-08-24 20:35:53 -0300

    fix: errors prettier format in deploy 3

--- pages/ParticleBackground.js
+++ pages/ParticleBackground.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Particles from 'react-particles-js'
-import ParticleConfig from './config/particle-config'
+import ParticleConfig from '../components/particle-config'
 
 export default function ParticleBackground() {
   return <Particles params={ParticleConfig}></Particles>
