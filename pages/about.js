49845788498d08a5c6a0c45ec187ece6bdb16d43
matheuslimat <matheuslimat@gmail.com>
2022-05-26 05:34:29 -0300

    fix 2

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
+import { useRouter } from 'next/router'
 
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
@@ -9,6 +10,12 @@ export async function getStaticProps() {
 }
 
 export default function About({ authorDetails }) {
+  const router = useRouter()
+
+  //Add this
+  if (!router.isFallback) {
+    console.log('xiiii')
+  }
   const { mdxSource, frontMatter } = authorDetails
 
   return (
